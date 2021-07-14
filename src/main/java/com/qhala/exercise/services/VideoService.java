0edadece33deda22feb01278173290cf660f19db
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-14 05:15:51 +0300

    Add method to add video

---
+++ src/main/java/com/qhala/exercise/services/VideoService.java
@@ -0,0 +1,21 @@
+package com.qhala.exercise.services;
+
+import com.qhala.exercise.entities.Video;
+import com.qhala.exercise.repositories.VideoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VideoService {
+
+    private final VideoRepository videoRepository;
+
+    public VideoService(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+    public Video addVideo(Video video){
+        return videoRepository.save(video);
+    }
+
+
+}
