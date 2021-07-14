c5a569ac8c9e09a6f85124294d025a9478db30df
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-14 06:01:41 +0300

    Add find user by username method

--- src/main/java/com/qhala/exercise/repositories/UserRepository.java
+++ src/main/java/com/qhala/exercise/repositories/UserRepository.java
@@ -4,4 +4,6 @@ import com.qhala.exercise.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository <User, Long>{
+
+    public Optional<User> findByUserName(String userName);
 }
