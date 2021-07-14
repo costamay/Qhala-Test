81d8f0202f52c12b3462955f6425b314358518a9
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-14 09:23:28 +0300

    Add EnableWebSecurity annotation

--- src/main/java/com/qhala/exercise/SecurityConfiguration.java
+++ src/main/java/com/qhala/exercise/SecurityConfiguration.java
@@ -4,10 +4,12 @@ import com.qhala.exercise.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
