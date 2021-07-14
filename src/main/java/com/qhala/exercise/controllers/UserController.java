59e0d9e2af32195d871a95c9478c026634dc3029
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-14 06:25:11 +0300

    Add get method return  all users

--- src/main/java/com/qhala/exercise/controllers/UserController.java
+++ src/main/java/com/qhala/exercise/controllers/UserController.java
@@ -3,6 +3,8 @@ package com.qhala.exercise.controllers;
 import com.qhala.exercise.entities.User;
 import com.qhala.exercise.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,4 +18,8 @@ public class UserController {
         return userService.createUser(user);
     }
 
+    @GetMapping("/all")
+    public List<User> getAll(){
+        return userService.getAll();
+    }
 }
