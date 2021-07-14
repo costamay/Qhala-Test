6b1b12b3d1bdb35ed451b5a7f082d01667f0bf0e
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-14 08:51:46 +0300

    Add MyUserDetail class to map the user data

--- src/main/java/com/qhala/exercise/entities/MyUserDetails.java
+++ src/main/java/com/qhala/exercise/entities/MyUserDetails.java
@@ -1,2 +1,52 @@
-package com.qhala.exercise.entities;public class MyUserDetails {
+package com.qhala.exercise.entities;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class MyUserDetails implements UserDetails {
+
+    private String userName;
+    private String password;
+
+    public MyUserDetails( User user){
+        this.userName = user.getUsernameName();
+        this.password = user.getPassword();
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
