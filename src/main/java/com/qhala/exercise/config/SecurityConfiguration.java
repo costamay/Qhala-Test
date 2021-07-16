e82f0c264f2092a4da4d7f394814fc27d8c4f008
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-16 07:49:52 +0300

    Define authenticationManager Bean

--- src/main/java/com/qhala/exercise/config/SecurityConfiguration.java
+++ src/main/java/com/qhala/exercise/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import com.qhala.exercise.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -28,6 +29,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/authenticate").permitAll()
                 .anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
     }
 
     @Bean
