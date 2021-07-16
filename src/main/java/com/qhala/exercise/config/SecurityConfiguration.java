c2c368f9322c6097b44250105c1a77244444225c
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-16 07:31:43 +0300

    Add config package

--- src/main/java/com/qhala/exercise/config/SecurityConfiguration.java
+++ src/main/java/com/qhala/exercise/config/SecurityConfiguration.java
@@ -3,12 +3,14 @@ package com.qhala.exercise.config;
 import com.qhala.exercise.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
