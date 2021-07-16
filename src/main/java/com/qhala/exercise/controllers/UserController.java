52d56e341fb918fdc58c99303b8fbd740da1391a
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-16 07:02:46 +0300

    Add authenticate endpoint

--- src/main/java/com/qhala/exercise/controllers/UserController.java
+++ src/main/java/com/qhala/exercise/controllers/UserController.java
@@ -1,22 +1,36 @@
 package com.qhala.exercise.controllers;
 
+import com.qhala.exercise.entities.AuthenticationRequest;
 import com.qhala.exercise.entities.User;
 import com.qhala.exercise.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
 //        Test method
-    @RequesMapping("/hello")
+    @RequestMapping("/hello")
     public String hello(){
         return  "Hello World";
+    }
+
+    //Authentication Endpoint
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+        );
     }
 
     @PostMapping("/create")
@@ -26,6 +40,7 @@ public class UserController {
 
     @GetMapping("/all")
     public List<User> getAll(){
+
         return userService.getAll();
     }
 }
