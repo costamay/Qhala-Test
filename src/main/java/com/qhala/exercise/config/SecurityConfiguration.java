e5028ef96bd16d3fb597a4ca2ef586f5b6622861
Nicholas Ngetich <ngetichnicholas903@gmail.com>
2021-07-16 08:34:34 +0300

    Add filter method befoere authentication and make session stateless

--- src/main/java/com/qhala/exercise/config/SecurityConfiguration.java
+++ src/main/java/com/qhala/exercise/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.qhala.exercise.config;
 
+import com.qhala.exercise.filter.JwtRequestFilter;
 import com.qhala.exercise.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +10,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +21,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Autowired
     UserService userService;
+
+    @Autowired
+    JwtRequestFilter jwtRequestFilter;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -28,7 +34,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected  void  configure(HttpSecurity http) throws Exception{
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/authenticate").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
